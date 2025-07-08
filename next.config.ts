8ae46d107abeaaa6d564f362c1f0162dfd560864
BİLAL ARDA AKSOY <bilpcukbilo@gmail.com>
2025-07-09 00:42:31 +0300

    TEST

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["picsum.photos"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"]
+  },
+  webpack: (config) => {
+    config.externals.push("@prisma/client");
+    return config;
+  }
 };
 
 export default nextConfig;
