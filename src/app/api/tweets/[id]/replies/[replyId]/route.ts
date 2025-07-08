8b40699418e06779a924f84a5783bebaf3dd9b0d
BİLAL ARDA AKSOY <bilpcukbilo@gmail.com>
2025-07-09 00:32:01 +0300

    pp

--- src/app/api/tweets/[id]/replies/[replyId]/route.ts
+++ src/app/api/tweets/[id]/replies/[replyId]/route.ts
@@ -3,6 +3,9 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
 // DELETE - Yanıtı sil
 export async function DELETE(
   request: NextRequest,
